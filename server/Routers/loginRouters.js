6aefea9bf1e2f7b276171920d2430dd1374712c5
Алкесандр <fos21@mail.ru>
2018-06-13 18:46:42 +0300

    server_001
    express session whis passport mysql express-session

--- server/Routers/loginRouters.js
+++ server/Routers/loginRouters.js
@@ -5,7 +5,8 @@ import { db_table, db } from '../../etc/config';
 const loginRoutes = Router();
 
 loginRoutes.post('/', (req, res)=>{
-    console.log(`SELECT * FROM \`${db.database}\`.\`${db_table.users}\` WHERE \`login\`='${req.body.login}' && \`password\`='${req.body.password}';`);
+
+
     connection.query(`SELECT * FROM \`${db.database}\`.\`${db_table.users}\` WHERE \`login\`='${req.body.login}' && \`password\`='${req.body.password}';`, function (error, results, fields) {
         if (error) throw error;
         if( results !== undefined ){
--- server/app.js
+++ server/app.js
@@ -1,56 +1,34 @@
 import express from 'express'; // Подкльчение модуля сервера
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import jwt from 'jsonwebtoken';
-import {serverPort, secret, rootRoutes} from '../etc/config.js';
-import loginRoutes from './Routers/loginRouters'
+import {serverPort} from '../etc/config.js';
+import session from 'express-session';
+import MySQLStore from 'mysql-express-session';
+
+const options = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'usbw',
+    database: 'onlinetester'
+};
+
+const sessionStore = new MySQLStore(options);
 
 const app = express();
-
+app.use(session({
+    key: 'session_cookie_name',
+    secret: 'session_cookie_secret',
+    store: sessionStore,
+    resave: true,
+    saveUninitialized: true
+}));
 app.use( bodyParser.json() );
 app.use(cors({ origin: '*' }));
 
-const requireToken = (req,res,next) => {
-  const token = req.headers.token;
-
-  jwt.verify(token, secret, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ error: err.message })
-    }
-    req.params.userId = decoded._id
-    next()
-  })
-}
-
 // RESTful api handlers
 
-app.use(`/${rootRoutes.auth}`, loginRoutes);
 
 const server = app.listen(serverPort, function() {
     console.log(`Server is up and running on port ${serverPort}`);
 });
-
-// app.post('/login', (req, res) => {
-//     db.listUsers().then(data => {
-//       [...data].map((el)=>{
-//         if(el.login === req.body.login && el.password === req.body.password){
-//                 let token =jwt.sign({id: data._id, sess: true}, secret);
-//           res.send({
-//                     isLogin: true,
-//                     token: token,
-//                     userName: el.name,
-//                 });
-//         }
-//       });
-//       res.send({
-//             isLogin: false
-//       });
-
-//     });
-// });
-
-// app.get('/',requireToken,(req, res) =>{
-//     db.upData(req.body.collectionName, req.body.data, req.body.id).then(data => {
-//         res.send(data);
-//     });
-// });
